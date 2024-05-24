c7a49e92749ce9d60313b93d74ffa1609055e936
Muneebshahid570 <168467556+Muneebshahid570@users.noreply.github.com>
2024-05-24 12:43:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Muneeb Shahid");
+console.log("Salman Shah");
+console.log('asad ali');
+console.log("Muneeb Shahid");
+let message = "Hello World"; // Infering Types, 
+//take your cursor on the variable name
+console.log('ahmed');
+console.log("mahroof");
---
+++ main.ts
@@ -0,0 +1,14 @@
+console.log("Muneeb Shahid");
+console.log("Salman Shah");
+console.log('asad ali');
+console.log("Muneeb Shahid");
+let message = "Hello World";// Infering Types, 
+                            //take your cursor on the variable name
+console.log('ahmed');
+console.log("mahroof");
+
+
+
+
+
+     
